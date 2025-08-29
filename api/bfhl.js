3468e3ff2e4f5830f6cd7cb775396abee62d281b
Varun <vbhalla230@gmail.com>
2025-08-29 11:14:13 +0530

    Fix 404 by moving handler into api/

--- api/bfhl.js
+++ api/bfhl.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { createServerlessHandler } from "@vercel/node"; // <-- trick
 
 const app = express();
 app.use(express.json());
@@ -7,4 +8,5 @@ app.post("/", (req, res) => {
   res.json({ ok: true, data: req.body });
 });
 
-export default app;
+// Export as handler for Vercel
+export default createServerlessHandler(app);
